1d83cf0f469fb4904e5c19533453b0084ca6cf4e
Lula <lucalula@gmail.com>
2013-08-27 22:00:39 +0200

    customer adapted

--- app/grids/addresses_short_grid.rb
+++ app/grids/addresses_short_grid.rb
@@ -5,5 +5,5 @@ class AddressesShortGrid
     Address
   end
   
-  column :full_description, header: I18n.t("mongoid.models.addresses", count: 1)
+  column :full_description, header: I18n.t("mongoid.models.address", count: 1)
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,28 @@
 module ApplicationHelper  
   
-  def panel(title, options, &block)
+  def panel(title, id, options={}, &block)
     heading = content_tag :div, class: "panel-heading" do
       content_tag :h3, class: "panel-title" do
+        
         links = []
         options[:links].each do |l|
           links << link_to(l.first, l.second)
+        end if options[:links]
+        
+        toggle_btn = content_tag :div, class: "pull-right" do
+          content_tag :a, class: "panel-collapse", href: "#", data: { toggle: "collapse", target: "##{id}" } do
+            content_tag :i, "", class: "icon-chevron-sign-up"
+          end
         end
-        "#{title} | ".concat(links.join("|")).html_safe
+        
+        "#{title} #{'| ' if options[:links]}".concat(links.join("|").concat(toggle_btn)).html_safe
       end
     end
     
     body = content_tag :div, class: "panel-body" do
-      capture(&block).html_safe
+      content_tag :div, id: "#{id}", class: "collapse in" do
+        capture(&block).html_safe
+      end
     end
     
     content_tag :div, class: "panel panel-default" do 
